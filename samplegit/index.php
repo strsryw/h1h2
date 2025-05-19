19308a6333030c1f2b0f15fe556cdadde4848db4
strsryw <satrio.suryo.wibowo@gmail.com>
2025-05-19 09:06:35 +0700

    link jquery ke project

--- samplegit/index.php
+++ samplegit/index.php
@@ -8,6 +8,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <script type="text/javascript" src="../functions/jquery/js/jquery.min.js"></script>
     <title>Sampel Git</title>
 </head>
 
