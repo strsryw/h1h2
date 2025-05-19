1e871ca24c7078ddb6f04c10cad3e50b8c5f3784
strsryw <satrio.suryo.wibowo@gmail.com>
2025-05-19 16:23:17 +0700

    selesai insert, update

--- samplegit/h2.php
+++ samplegit/h2.php
@@ -27,6 +27,49 @@ if (!empty($mode)) {
 
         echo $txtData;
     } else if ($mode == "simpan") {
-        var_dump($_POST);
+        $id = $_POST['id'];
+        $nama = $_POST['nama'];
+        $status = $_POST['status'];
+        $aktif = $_POST['aktif'];
+        $cons = $_POST['cons'];
+
+
+        try {
+            $dbCon->execute("BEGIN");
+
+            if ($id == 0 || $id == '') {
+                $rsCek = $dbCon->execute("SELECT * FROM dpfdplnew.distributors d WHERE d.nama='$nama' AND d.aktif='true'");
+                $jumlahCek = $dbCon->getNumRows($rsCek);
+
+                if ($jumlahCek == 0) {
+                    $querisimpan = "INSERT INTO dpfdplnew.distributors(nama, status, aktif, cons) VALUES ('$nama','$status', '$aktif', '$cons')";
+                    $rsPosSimpan = $dbCon->execute($querisimpan);
+                    if (!$rsPosSimpan) {
+                        echo "gagal";
+                    } else {
+                        echo "sukses";
+                    }
+                } else {
+                    echo "gagal";
+                }
+            } else {
+                $rsCek = $dbCon->execute("SELECT * FROM dpfdplnew.distributors d WHERE d.nama='$nama' AND d.aktif='true' AND id != '$id'");
+                $jumlahCek = $dbCon->getNumRows($rsCek);
+                if ($jumlahCek == 0) {
+                    $rsUpdatenmMerk = $dbCon->execute("UPDATE dpfdplnew.distributors SET nama='$nama', status='$status', aktif = '$aktif', cons = '$cons' WHERE id='$id'");
+                    if (!$rsUpdatenmMerk) {
+                        echo "gagal";
+                    } else {
+                        echo "sukses";
+                    }
+                } else {
+                    echo "gagal";
+                }
+            }
+            $dbCon->execute("COMMIT");
+        } catch (Exception $e) {
+            var_dump($e->getTrace());
+            $dbCon->execute("ROLLBACK");
+        }
     }
 }
